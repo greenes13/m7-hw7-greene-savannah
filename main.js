a715d9774028f097107fa63bd31b160d410d94f5
Savannah  Greene <savannahgreene@Savannahs-MBP-2.attlocal.net>
2019-02-21 20:42:48 -0600

    Add API, fix html and css code issues

--- main.js
+++ main.js
@@ -1,9 +1,17 @@
+var cityWrapper = document.querySelectorAll('#city-wrapper h1 span');
+var weatherWrapper = document.querySelectorAll('#weather-wrapper p span');
+
 var xmlhttp = new XMLHttpRequest();
 xmlhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
 
         var apiResult = JSON.parse(this.responseText);
-        //Code Dealing With The API Data Goes Here (inside function)
+
+        var cityName = document.createTextNode(apiResult.name);
+        cityWrapper[0].appendChild(cityName);
+
+        var weatherType = document.createTextNode(apiResult.weather[0].main);
+        weatherWrapper[0].appendChild(weatherType);
   }
 };
 xmlhttp.open('GET', 'http://api.openweathermap.org/data/2.5/weather?q=70130,us&appid=6efff70fe1477748e31c17d1c504635f', true);
